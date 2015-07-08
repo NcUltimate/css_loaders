6d623564a269f031291e42795cee30c2dd69a92e
Joseph Klemen <jklemen@enova.com>
2015-07-08 15:03:51 -0500

    added loader titles.

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 
 set :port, 9494
 
+loaders = ['07-05: Corners', '07-06: Portal', '07-07: Pulse']
 get '/' do
-  haml :index, :locals => {:num_loaders => 3}
+  haml :index, :locals => {:num_loaders => 3, :loaders => loaders}
 end
