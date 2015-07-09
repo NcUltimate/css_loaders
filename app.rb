b6726a961eb6105194971f24423a21faa2f3cb83
Joseph Klemen <jklemen@enova.com>
2015-07-09 11:16:26 -0500

    loader06

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'sinatra'
 set :port, 9494
 
 loaders = ['07-05: Corners', '07-06: Portal', '07-07: Pulse']
+loaders += ['07-08: Clock', '07-09: Splash', '07-10: Paddle']
 get '/' do
-  haml :index, :locals => {:num_loaders => 3, :loaders => loaders}
+  haml :index, :locals => {:num_loaders => loaders.count, 
+                          :loaders => loaders}
 end
