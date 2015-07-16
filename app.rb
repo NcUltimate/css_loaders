734943c309674b77e45cd98dbd0e23d240ff327d
Joseph Klemen <jklemen@enova.com>
2015-07-16 10:54:39 -0500

    Added loaders 15 - 17. Working on 18. Added devices graphic.

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ set :port, 9494
 
 loaders = JSON.parse(open('loaders.json', &:read))
 get '/' do
+  demo_sizes = %w(small normal large)
   haml :index, :locals => {:num_loaders => loaders.count, 
-                          :loaders => loaders['loaders']}
+                          :loaders => loaders['loaders'],
+                          :sizes => demo_sizes}
 end
