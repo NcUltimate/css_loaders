35f321a8326c3e3b220308134fe9d337ac68b248
Joseph Klemen <jklemen@enova.com>
2015-07-22 10:21:33 -0500

    Ready for deployment to heroku.

--- app.rb
+++ app.rb
@@ -1,12 +1,17 @@
 require 'sinatra'
+require 'sinatra/base'
 require 'JSON'
 
-set :port, 9494
+class LoadingCSS < Sinatra::Base
 
-loaders = JSON.parse(open('loaders.json', &:read))
-get '/' do
-  demo_sizes = %w(small normal large)
-  haml :index, :locals => {:num_loaders => loaders.count, 
-                          :loaders => loaders['loaders'],
-                          :sizes => demo_sizes}
+  loaders = JSON.parse(open('loaders.json', &:read))
+
+  set :port, 9494
+
+  get '/' do
+    demo_sizes = %w(small normal large)
+    haml :index, :locals => {:num_loaders => loaders.count, 
+                            :loaders => loaders['loaders'],
+                            :sizes => demo_sizes}
+  end
 end
